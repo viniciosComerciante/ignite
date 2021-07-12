40febc7583b67eedd156ff97ef392f4105c97565
viniciosComerciante <vinicompif@gmail.com>
2021-07-12 14:00:32 -0300

    adicionando typescript na aplicaçao

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,5 +1,6 @@
-import '../src/styles/global.scss';
+import "../src/styles/global.scss";
+import { RepositoryList } from "./components/RepositoryList";
 
-export function App(){
-    return <h1>Hello World</h1>
+export function App() {
+  return <RepositoryList></RepositoryList>;
 }
---
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+export function RepositoryItem(props) {
+  return (
+    <li>
+      <strong>{props.repository?.name ?? "default"}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>Acessar Repositório</a>
+    </li>
+  );
+}
