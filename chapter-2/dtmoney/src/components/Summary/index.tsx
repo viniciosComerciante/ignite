e18a1e6e38c4efa1da31fae3144d4ad014729f0a
viniciosComerciante <vinicompif@gmail.com>
2021-07-13 17:02:49 -0300

    criada dashboard, começando a criar a tabela de transaçoes

---
+++ chapter-2/dtmoney/src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="entradas" />
+        </header>
+
+        <strong>R$ 1000</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="entradas" />
+        </header>
+
+        <strong>- R$ 500</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="entradas" />
+        </header>
+
+        <strong>R$ 500</strong>
+      </div>
+    </Container>
+  );
+}
