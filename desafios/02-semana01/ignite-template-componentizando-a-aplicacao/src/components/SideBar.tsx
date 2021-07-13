17bc01d04bd15dba214fe25659bdc99fdb96910a
viniciosComerciante <vinicompif@gmail.com>
2021-07-13 01:43:56 -0300

    finalizado desafio

--- desafios/02-semana01/ignite-template-componentizando-a-aplicacao/src/components/SideBar.tsx
+++ desafios/02-semana01/ignite-template-componentizando-a-aplicacao/src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import {Button} from '../components/Button';
+
+type Genres = {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+}
+
+interface SidebarProps{
+  genres: Genres[];
+  handleClick: (id:number)=>void;
+  selectedGenreId: number;
+}
+
+export function SideBar({genres, handleClick, selectedGenreId}: SidebarProps) {
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClick(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
